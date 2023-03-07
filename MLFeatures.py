656ba71198c36b29507f817c67a0f0fe1824ec72
Jed Gore <jedgore@Jeds-Mac-mini.local>
2023-03-07 18:57:13 -0500

    prophet

--- MLFeatures.py
+++ MLFeatures.py
@@ -1,7 +1,25 @@
 from dataclasses import MISSING, dataclass, field
+from scipy import signal
+import numpy as np
+from numpy.random import default_rng
+import pandas as pd
+from matplotlib import pyplot
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 class FeatureConfig:
+    # trend
+    # season
+    # cycle
+
+    def detrend(self, x):
+        y = signal.detrend(x, axis=-1, type="constant", bp=0)
+        return y
+
+    def deseasonalize(sefl, data):
+        decompose_data = seasonal_decompose(data, model="additive")
+        return decompose_data
+
     pass
 
 
@@ -40,3 +58,7 @@ class MLForecast:
 
     def feature_importance(self):
         pass
+
+
+if __name__ == "__main__":
+    pass
--- MLTest.py
+++ MLTest.py
@@ -1,31 +1,32 @@
-# Data manipulation
-# ==============================================================================
-import numpy as np
-import pandas as pd
-
-# Plots
-# ==============================================================================
-import matplotlib.pyplot as plt
-import seaborn as sns
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.graphics.tsaplots import plot_pacf
-
-plt.style.use("fivethirtyeight")
-
-# Modelling and Forecasting
-# ==============================================================================
-from sklearn.linear_model import Ridge
-
-# from lightgbm import LGBMRegressor
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error
-from skforecast.ForecasterAutoreg import ForecasterAutoreg
-from skforecast.ForecasterAutoregDirect import ForecasterAutoregDirect
-from skforecast.model_selection import grid_search_forecaster
-from skforecast.model_selection import backtesting_forecaster
-
-# Warnings configuration
-# ==============================================================================
 import warnings
 
-warnings.filterwarnings("ignore")
+warnings.simplefilter("ignore")
+#!pip install pystan
+#!pip install prophet
+# https://gist.github.com/thewisenerd/52f937d01b06287ccf21a05a118e74ad
+import pandas as pd
+from prophet import Prophet
+
+df = pd.read_csv("dataset.csv")
+
+# some munging
+df["Year"] = df["Time Date"].apply(lambda x: str(x)[-4:])
+df["Month"] = df["Time Date"].apply(lambda x: str(x)[-6:-4])
+df["Day"] = df["Time Date"].apply(lambda x: str(x)[:-6])
+df["ds"] = pd.DatetimeIndex(df["Year"] + "-" + df["Month"] + "-" + df["Day"])
+df = df.loc[(df["Product"] == 2667437) & (df["Store"] == "QLD_CW_ST0203")]
+df.drop(["Time Date", "Product", "Store", "Year", "Month", "Day"], axis=1, inplace=True)
+df.columns = ["y", "ds"]
+df.head()
+
+
+m = Prophet(interval_width=0.95, daily_seasonality=True)
+model = m.fit(df)
+
+future = m.make_future_dataframe(periods=100, freq="D")
+forecast = m.predict(future)
+print(forecast.tail())
+
+plot1 = m.plot(forecast)
+
+plt2 = m.plot_components(forecast)
